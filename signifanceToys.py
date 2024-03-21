31a7b8a310cb9608f8363d73c91522454f717c0c
Anthony Badea <badeaa3@gmail.com>
2024-03-21 10:38:50 +0100

    update

--- signifanceToys.py
+++ signifanceToys.py
@@ -2,24 +2,58 @@ from utils.plot import plot, text
 import matplotlib.pyplot as plt
 
 
-# PbPb v23
+# PbPbv23
 ntoys = [10**0, 10**1, 10**2, 10**3, 10**4, 5*10**4, 10**5]
 significance = [0, 0, 0, 3.090232, 3.719016, 3.598547, 3.540084]
 yerr = [0, 0, 0, 0.296844, 0.252610, 0.091958, 0.059006]
 asymptotic = 3.434812
-fig, ax = plot(x=ntoys, y=significance, yerr=yerr, xlim=[10**0, 2*10**5], xlog=True, ylim=[0, 4.5], xlabel="Number of Toys", ylabel="Significance", label="PbPbv23", color="red")
-plt.hlines(asymptotic, 10**0, 2*10**5, color="red")
-text(0.075, 0.81, f"Asymptotics = {asymptotic:.2f}", fontsize=12, color="red")
+fig, ax = plot(x=ntoys, y=significance, yerr=yerr, xlim=[10**0, 2*10**5], xlog=True, ylim=[0, 4.5], xlabel="Number of Toys", ylabel="Significance", label=f"v23 ({asymptotic:.2f})", color="red")
+plt.hlines(asymptotic, 10**0, 2*10**5, color="red", ls="--", alpha=0.5)
+#text(0.075, 0.81, f"Asymptotics = {asymptotic:.2f}", fontsize=12, color="red")
 
+# PbPbv25
+ntoys = [10**0, 10**1, 10**2, 10**3, 10**4, 5*10**4, 10**5]
+# expected with data
+significance = [0, 0, 0, 0, 3.290527, 3.258507, 3.301994]
+yerr = [0, 0, 0, 0, 0.125789, 0.053607, 0.040476]
+asymptotic = 3.269032
+# asimov
+significance = [0, 0, 0, 0, 3.290527, 3.258507, 3.301994]
+yerr = [0, 0, 0, 0, 0.125789, 0.053607, 0.040476]
+asymptotic = 3.269032
+# plot
+color = "purple"
+plt.errorbar(ntoys, significance, xerr=None, yerr=yerr, fmt='-o', color=color, label=f"v25 ({asymptotic:.2f})")
+plt.hlines(asymptotic, 10**0, 2*10**5, color=color, ls="--", alpha=0.5)
+#text(0.075, 0.88, f"Asymptotics = {asymptotic:.2f}", fontsize=12, color=color)
 
-# PbPb v28
+# PbPbv27
+ntoys = [10**0, 10**1, 10**2, 10**3, 10**4, 5*10**4, 10**5]
+# expected with data
+significance = [0, 0, 0, 0, 3.540084, 3.598547, 3.633134]
+yerr = [0, 0, 0, 0, 0.186592, 0.091958, 0.068928]
+asymptotic = 3.629660
+# asimov
+significance = [0, 0, 0, 0, 3.540084, 3.598547, 3.633134]
+yerr = [0, 0, 0, 0, 0.186592, 0.091958, 0.068928]
+asymptotic = 3.629660
+# plots
+color = "green"
+plt.errorbar(ntoys, significance, xerr=None, yerr=yerr, fmt='-o', color=color, label=f"v27 ({asymptotic:.2f})")
+plt.hlines(asymptotic, 10**0, 2*10**5, color=color, ls="--", alpha=0.5)
+#text(0.075, 0.88, f"Asymptotics = {asymptotic:.2f}", fontsize=12, color=color)
+
+# PbPbv28
 ntoys = [10**0, 10**1, 10**2, 10**3, 10**4, 5*10**4, 10**5]
 significance = [0, 0, 0, 0, 3.719016, 3.846126, 3.846126]
 yerr = [0, 0, 0, 0, 0.252610, 0.141533, 0.100079]
 asymptotic = 3.740695
-plt.errorbar(ntoys, significance, xerr=None, yerr=yerr, fmt='-o', color="black", label="PbPbv28")
-plt.hlines(3.740695, 10**0, 2*10**5, color="black")
-text(0.075, 0.88, f"Asymptotics = {asymptotic:.2f}", fontsize=12, color="black")
+color = "black"
+plt.errorbar(ntoys, significance, xerr=None, yerr=yerr, fmt='-o', color=color, label=f"v28 ({asymptotic:.2f})")
+plt.hlines(asymptotic, 10**0, 2*10**5, color=color, ls="--", alpha=0.5)
+#text(0.075, 0.88, f"Asymptotics = {asymptotic:.2f}", fontsize=12, color=color)
 
-plt.legend(loc="lower left", fontsize=18)
+plt.legend(title="Sample (Asymptotic)", loc="lower left", fontsize=14, title_fontsize=14)
+text(0.04, 0.96, text="Expected significance using data", fontsize=15, color="black")
+text(0.04, 0.91, text="Significance roughly when using asimov dataset", fontsize=12, color="black")
 plt.savefig("ttPbPbToys.pdf", bbox_inches="tight")
